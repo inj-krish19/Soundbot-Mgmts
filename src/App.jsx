e961606b9b829cdc1192fdc141112f2a831151db
inj-krish19 <kglivee19@gmail.com>
2026-01-30 11:38:15 +0530

    Defining Routes for Sign In, Sign Up and Change Password Proceed Page and Calling me route for authentication status and storing on AuthStore

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,68 @@ import './index.css'
 import ThemeToggle from './components/ThemeToggle'
 import BackToTop from './components/BackToTop'
 
+import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
-
 
 
 
 import Header from './components/Header';
 
+
+import Verification from './pages/Verification';
+import ResetPassword from './pages/ResetPassword';
+
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import useAuth from './store/AuthStore';
+import { useEffect } from 'react';
 
 function App() {
+
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+  const setAuth = useAuth((state) => state.setAuth);
+
+  useEffect(() => {
+
+    (async () => {
+
+      let response = await fetch(`${BACKEND_URL}/auth/me`, {
+        method: 'POST',
+        headers: {
+          "content-type": "application/json"
+        },
+        credentials: 'include'
+      });
+
+      let res = await response.json();
+      setAuth(res['authenticated']);
+
+      console.log(res);
+
+    })();
+
+  }, []);
 
   return (
     <>
 
-      <Header />
-
       <Router>
+
+        <Header />
         <Routes>
 
-          <Route path='/signin' element={<SignIn />} />
+          <Route path='/verification' element={<Verification />} />
+          <Route path='/auth/signup/:hash' element={<SignUp />} />
+          <Route path='/auth/signin/:hash' element={<SignIn />} />
+
+          <Route path='/auth/reset-password/:hash' element={<ResetPassword />} />
 
         </Routes>
+
+        <BackToTop />
+        <ThemeToggle />
+
       </Router>
-
-
-      <BackToTop />
-      <ThemeToggle />
 
     </>
   )
