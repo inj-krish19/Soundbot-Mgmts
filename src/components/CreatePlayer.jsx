bc9a7226608028e775af567b8a93ca972a412f11
inj-krish19 <kglivee19@gmail.com>
2026-02-06 18:03:12 +0530

    Fixing Select Player and Handling No Player found testcase

--- src/components/CreatePlayer.jsx
+++ src/components/CreatePlayer.jsx
@@ -107,13 +107,13 @@ function CreatePlayer({ panel }) {
 
     return (
         <>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 flex flex-col gap-8 w-3/4 md:w-1/3 h-auto bg-stone-300 dark:bg-stone-700 p-4 rounded-md ">
-
-                <ImCross className="absolute top-3 right-3 text-rose-400" onClick={() => panel(false)} />
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-3/5 flex flex-col gap-8 w-3/4 md:w-1/3 h-auto bg-stone-300 dark:bg-stone-700 p-4 rounded-md z-100">
 
                 <span className="text-sky-400 font-bold font-poppis text-lg">Create Player</span>
 
+                <ImCross className="absolute top-3 right-3 text-rose-400" onClick={() => panel(false)} />
                 <Notification info={info} />
+
                 <form className="flex flex-col flex-wrap justify-around gap-4" onSubmit={(e) => { submitPlayer(e) }}>
 
                     <div className="flex flex-col justify-center gap-1">
@@ -134,19 +134,19 @@ function CreatePlayer({ panel }) {
                     <div className="flex flex-col justify-center gap-1">
                         <label htmlFor="type" className="text-slate-700 dark:text-slate-300 text-sm">Type</label>
                         <Listbox value={type} onChange={setType} >
-                            <ListboxButton className="flex items-center gap-2 border px-4 py-2 border-2 outline-slate-100 border-slate-100 text-slate-900 dark:text-slate-100 dark:text-slate-100 rounded-sm capitalize" id="type" name="type">
+                            <ListboxButton className="px-4 py-2 border-2 border-slate-100 outline-slate-100 rounded-sm flex items-center gap-2 capitalize" id="type" name="type">
                                 {getSVGByPlayerType(type, 'text-purple-400')}
                                 <span className="text-fuchsia-400">{type}</span>
                             </ListboxButton>
 
-                            <ListboxOptions className="px-2 py-1 border borde-blue-500 text-slate-900 dark:text-slate-100 hover:outlie dark:text-slate-100 rounded-sm capitalize">
+                            <ListboxOptions className="border-2 border-slate-100 rounded-sm ">
                                 {players.map((player) => (
                                     <ListboxOption
                                         key={player}
                                         value={player}
-                                        className="flex items-center gap-2 px-4 py-2 hover:bg-pink-200 cursor-pointer"
+                                        className="px-4 py-2 flex items-center gap-2 hover:bg-pink-200 hover:cursor-pointer capitalize"
                                     >
-                                        {getSVGByPlayerType(player, 'text-purple-400   ')}
+                                        {getSVGByPlayerType(player, 'text-purple-400 ')}
                                         <span className="text-purple-500">{player}</span>
                                     </ListboxOption>
                                 ))}
@@ -168,7 +168,7 @@ function CreatePlayer({ panel }) {
                         </div>
                     </div>
 
-                    <button type="submit" className="p-2 text-slate-800 bg-violet-500 rounded-md font-bold font-poppins">Submit</button>
+                    <button type="submit" className="p-2 bg-violet-500 text-slate-200 rounded-md font-bold font-poppins">Submit</button>
 
                 </form>
 
--- src/components/CreateSession.jsx
+++ src/components/CreateSession.jsx
@@ -1,25 +1,77 @@
 import { ImCross } from "react-icons/im";
 import Notification from "./Notification";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getSVGByPlayerType } from './CreatePlayer'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, ListboxSelectedOption } from "@headlessui/react";
+import { BACKEND_URL } from "../store/UrlStore";
 
 function CreateSession({ panel }) {
 
+    const [players, setPlayers] = useState([]);
+
+    const [player, setPlayer] = useState(null);
     const [info, setInfo] = useState({
         message: '',
         type: ''
-    })
+    });
+
+
+    useEffect(() => {
+        (async () => {
+            let response = await fetch(`${BACKEND_URL}/player/`, {
+                method: 'GET',
+                headers: {
+                    "content-type": "application/json"
+                },
+                credentials: 'include'
+            });
+
+            let res = await response.json();
+            console.log("Answer", res['data']);
+
+            setPlayers(res.data);
+            setPlayer(res.data?.[0] || null)
+
+        })();
+    }, [])
 
     return (
         <>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-2/3 flex flex-col gap-4 items-center bg-stone-300 dark:bg-stone-700 rounded-md p-4">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-3/5 flex flex-col gap-8 w-3/4 md:w-1/3 h-auto bg-stone-300 dark:bg-stone-700 rounded-md p-4 z-100">
+
                 <span className="text-emerald-400 text-lg font-bold">Create Session</span>
 
                 <ImCross className="absolute top-3 right-3 text-rose-400" onClick={() => { panel(false); }} />
                 <Notification info={info} />
 
-                <div className="flex flex-col items-center gap-4">
+                <div className="flex flex-col items-center gap-4 ">
 
-                    <form className="flex flex-col justify-around gap-4" onSubmit={() => { }}>
+                    <form className="flex flex-col justify-around gap-4 w-full" onSubmit={() => { }}>
+
+                        <div className="flex flex-col gap-1">
+                            <label htmlFor="player" className="text-slate-700 dark:text-slate-300 text-sm">Player</label>
+                            <Listbox value={player} onChange={setPlayer}>
+
+                                {(!player || player.length === 0) && <span className="text-rose-500 text-md">Please add streaming player first</span>}
+
+                                {player && <ListboxButton className="px-4 py-2 border-2 border-slate-100 outline-slate-100 rounded-sm flex items-center gap-2 ">
+                                    {getSVGByPlayerType(player.type, 'text-purple-400')}
+                                    <span className="text-fuchsia-400">{player.nickname}</span>
+                                </ListboxButton>}
+
+
+                                {players.length !== 0 && <ListboxOptions className='border-2 border-slate-100 rounded-sm'>
+                                    {players.map(player => {
+                                        return (
+                                            <ListboxOption key={player.id} value={player} className='px-4 py-2 flex items-center gap-2 hover:bg-pink-200 hover:cursor-pointer'>
+                                                {getSVGByPlayerType(player.type, 'text-purple-400')}
+                                                <span className="text-purple-500 ">{player.nickname}</span>
+                                            </ListboxOption>
+                                        )
+                                    })}
+                                </ListboxOptions>}
+                            </Listbox>
+                        </div>
 
                         <div className="flex flex-col gap-1 ">
                             <label htmlFor="startDate" className="text-slate-700 dark:text-slate-300 text-sm ">Start Date</label>
@@ -50,16 +102,16 @@ function CreateSession({ panel }) {
 
                         <div className="flex flex-col gap-1">
                             <label htmlFor="note" className="text-slate-700 dark:text-slate-300 text-sm">Note</label>
-                            <input type="text" inputMode="numeric" pattern="[0-9]*" className="px-2 py-1 border-2 border-slate-200 outline-slate-200 rounded-sm text-slate-800 dark:text-slate-200" placeholder="ex. Listening Eminem" />
+                            <input type="text" name="note" id="note" inputMode="numeric" pattern="[0-9]*" className="px-2 py-1 border-2 border-slate-200 outline-slate-200 rounded-sm text-slate-800 dark:text-slate-200" placeholder="ex. Listening Eminem" />
                         </div>
 
                         <button type="submit" className="p-2 bg-violet-500 text-slate-200 rounded-md font-bold font-poppins">Submit</button>
 
                     </form>
 
-                </div>
+                </div >
 
-            </div>
+            </div >
         </>
     )
 
