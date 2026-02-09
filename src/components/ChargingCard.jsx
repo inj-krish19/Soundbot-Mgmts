af68148046f1b6bb3fcfdfc9e9f06a69e844cec7
inj-krish19 <kglivee19@gmail.com>
2026-02-09 19:18:02 +0530

    Defining Charging Card for Charging Information and Enhancement in Session Card

---
+++ src/components/ChargingCard.jsx
@@ -0,0 +1,32 @@
+import { TbRecharging } from 'react-icons/tb'
+
+function ChargingCard({ charging }) {
+
+    return (
+        <>
+            <div className="absolute top-1/2 left-1/2 -translate-1/2 flex flex-col gap-4 p-4 bg-stone-300 dark:bg-stone-700 w-3/4 md:w-1/3 h-auto rounded-md">
+
+                <div className="flex flex-row gap-2">
+                    <TbRecharging size={24} className='text-violet-400 dark:text-indigo-400' />
+                    <span className='text-slate-800 dark:text-slate-200 font-bold font-poppins'>Charging</span>
+                </div>
+
+                <div className="flex flex-col gap-1">
+                    {/* <p className='text-slate-700 dark:text-slate-300 text-sm'>ID: <span className="font-bold font-poppins" >{charging._id}</span></p> */}
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Charging Start Date: <span className="font-bold font-poppins" >{charging.chargingStartDate}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Charging End Date: <span className="font-bold font-poppins" >{charging.chargingEndDate}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Charging Start Time: <span className="font-bold font-poppins" >{charging.chargingStartTime}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Charging End Time: <span className="font-bold font-poppins" >{charging.chargingEndTime}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>First Session Date: <span className="font-bold font-poppins" >{charging.firstSessionDate}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Last Session Date: <span className="font-bold font-poppins" >{charging.lastSessionDate}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Charging Duration: <span className="font-bold font-poppins" >{charging.chargingDuration}</span></p>
+                    <p className='text-slate-700 dark:text-slate-300 text-sm'>Note: <span className="font-bold font-poppins" >{charging.note || "-"}</span></p>
+                </div>
+
+            </div>
+        </>
+    )
+
+}
+
+export default ChargingCard;
--- src/components/SessionCard.jsx
+++ src/components/SessionCard.jsx
@@ -4,7 +4,7 @@ function SessionCard({ session }) {
 
     return (
         <>
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 flex flex-col gap-4 p-4 rounded-md bg-stone-300 dark:bg-stone-700 w-3/4 md:w-1/3 h-auto z-100">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 flex flex-col gap-4 p-4 rounded-md bg-stone-300 dark:bg-stone-700 w-3/4 md:w-1/3 h-auto z-100">
 
                 <div className="flex flex-row items-center gap-2">
                     <SiSession className="text-teal-400 dark:text-purple-500" />
