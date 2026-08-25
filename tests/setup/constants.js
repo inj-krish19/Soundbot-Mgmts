134e411866293c84bd039fea80eed06202cf1933
inj-krish19 <kglivee19@gmail.com>
2026-08-25 17:54:25 +0530

    test: creating constants and setup test files

---
+++ tests/setup/constants.js
@@ -0,0 +1,11 @@
+export const TEST_USER = {
+    id: 'test-user-id',
+    name: 'Test User',
+    email: 'test@example.com',
+    nickname: 'Tester',
+};
+
+export const VALID_PLAYER_TYPES = ['earbud', 'earphone', 'headphone'];
+export const VALID_DEVICE_TYPES = ['phone', 'tablet', 'desktop', 'others'];
+
+export const BACKEND_URL = 'http://localhost:8000';
